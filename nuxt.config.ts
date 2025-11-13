d2446aee3937ddd821db2be9beab05058ec47254
Marko Banušić <mbanusic@gmail.com>
2025-11-13 09:27:43 +0100

    preset fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/ui"]
+  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/ui"],
+    nitro: {
+        preset: 'cloudflare-pages',
+        routeRules: {
+            '/**': {
+                headers: {
+                    'X-Clacks-Overhead': 'GNU Terry Pratchett',
+                    'X-Jobs': 'Want to works with us? Contact us at info@sporty.plus'
+                }
+            }
+        }
+    },
 })
